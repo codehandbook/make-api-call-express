28a9b9e3a13e05e9ac79b6c160a4ad1998de9d96
ak7883 <ak7883@gmail.com>
2018-09-19 19:16:29 +0530

    adding project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const api_helper = require('./API_helper')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Welcome to Make REST API Calls In Express!'))
+
+app.get('/getAPIResponse', (req, res) => {
+	api_helper.make_API_call('https://jsonplaceholder.typicode.com/todos/1')
+	.then(response => {
+		res.json(response)
+	})
+	.catch(error => {
+		res.send(error)
+	})
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
